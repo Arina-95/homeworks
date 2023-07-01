3653694d7a6d6ffc49cfd08474ef96f8c752e9fa
Arina Lobanovskaya <arina.working@gmail.com>
2023-07-01 03:14:31 +0300

    6th in progress

---
+++ 6/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] AskArray (int size)
+{
+    System.Console.WriteLine("Введите числа в массив");
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+void CountPos (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"В массиве {count} чисел больше 0 ");
+}
+
+System.Console.WriteLine("Введите размер массива -> ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] myArray = AskArray(size);
+PrintArray(myArray);
+System.Console.WriteLine();
+CountPos(myArray);
+
+
+
+
+// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
